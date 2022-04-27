3aad4f8e0fa6a3bf2fa03348cf6fefd602c46d47
Boburuu <camille.disera.30@gmail.com>
2022-04-27 15:09:59 +0200

    ok

--- itfs/src/app/app-routing.module.ts
+++ itfs/src/app/app-routing.module.ts
@@ -5,8 +5,14 @@ import { HomeComponent } from './pages/home/home.component';
 import { FormationComponent } from './pages/formation/formation.component';
 import { AuditComponent } from './pages/audit/audit.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { from } from 'rxjs';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'formation', component:FormationComponent},
+  {path:'audit', component:AuditComponent},
+  {path:'contact', component:ContactComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
