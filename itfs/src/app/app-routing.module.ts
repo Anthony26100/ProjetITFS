8fdbbae126761c5cb813bd701d3cf382e3bebfcc
PetitCakeAuxOlives <morgan.rdr@gmail.com>
2022-04-27 12:53:47 +0200

    a

--- itfs/src/app/app-routing.module.ts
+++ itfs/src/app/app-routing.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { HomeComponent } from './pages/home/home.component';
+import { FormationComponent } from './pages/formation/formation.component';
+import { AuditComponent } from './pages/audit/audit.component';
+import { ContactComponent } from './pages/contact/contact.component';
+
 const routes: Routes = [];
 
 @NgModule({
--- itfs/src/app/app.module.ts
+++ itfs/src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { FormationComponent } from './formation/formation.component';
-import { AuditComponent } from './audit/audit.component';
-import { ContactComponent } from './contact/contact.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FormationComponent } from './pages/formation/formation.component';
+import { AuditComponent } from './pages/audit/audit.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
